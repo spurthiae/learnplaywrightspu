42ea8a0285c835877c4c2fdba0c4421e40901cd8
Shashi P <shaship@Shashis-Mac-mini.local>
2026-09-15 23:53:38 +0530

    Updated GreenKart test

---
+++ tests/pj/ass2.spec.js
@@ -0,0 +1,45 @@
+import { test, expect } from '@playwright/test';
+
+test('GreenKart', async ({ page }) => {
+  await page.goto('https://rahulshettyacademy.com/seleniumPractise/#/');
+
+  const product1 = page.locator(
+    "xpath=//div[@class='product' and .//img[@alt='Almonds - 1/4 Kg']]"
+  );
+
+  const product2 = page.locator(
+    "xpath=//div[@class='product' and .//img[@alt='Pomegranate - 1 Kg']]"
+  );
+
+  
+  await product1.locator("button").click();
+  await product2.locator("button").click();
+
+  
+  const price1 = Number(
+    await product1.locator(".product-price").textContent()
+  );
+
+  const price2 = Number(
+    await product2.locator(".product-price").textContent()
+  );
+
+  
+  const totalPrice = price1 + price2;
+
+  console.log("Price1:", price1);
+  console.log("Price2:", price2);
+  console.log("Total Price:", totalPrice);
+
+  
+  await page.locator("xpath=//img[@alt='Cart']").click();
+
+  
+  const cartTotal = Number(
+    await page.locator("xpath=(//div[@class='cart-info']//strong)[2]").textContent()
+  );
+
+  console.log("Cart Total:", cartTotal);
+
+  expect(cartTotal).toBe(totalPrice);
+});
