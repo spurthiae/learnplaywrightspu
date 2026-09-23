71085a338aa2ac1a80599ca14c45c318f51f6b37
Shashi P <shaship@Shashis-Mac-mini.local>
2026-09-23 13:30:35 +0530

    new upload

---
+++ learnjs/variables.spec.js
@@ -0,0 +1,34 @@
+
+
+
+
+//if(true){
+
+//var a = "spu";
+//console.log(a)  
+
+
+//}
+
+
+//if (true){
+    
+  //  console.log(a) // a is accessible here because var is function scoped   
+//}
+
+//#2
+
+//if(true){
+
+//var a = "spu";
+//console.log(a)  
+// a = "sunny"
+
+//}
+
+
+//if (true){
+    
+  //  console.log(a) // a is accessible here because var is function scoped   
+//} 
+
